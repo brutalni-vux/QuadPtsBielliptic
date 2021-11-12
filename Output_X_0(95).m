5115c36a164e6c3cc3592aa22bafc00c5bd0764b
brutalni-vux <36900101+brutalni-vux@users.noreply.github.com>
2021-11-12 20:28:03 +0100

    Add files via upload

---
+++ Output_X_0(95).m
@@ -0,0 +1,165 @@
+Magma V2.26-3     Fri Nov 12 2021 20:12:32 on euler    [Seed = 2641510470]
+Type ? for help.  Type <Ctrl>-D to quit.
+Loading "quadptssieve.m"
+Loading "ozmansiksek.m"
+Loading "X0N_NiceModel.m"
+Dimension of CuspForms(95) is:
+9
+Dimesion of eigenspace lambda=1 for w19 is:
+3
+Dimesion of eigenspace lambda=-1 for w19 is:
+6
+Model for X95 is:
+Curve over Rational Field defined by
+x[1]^2 + 4*x[3]*x[4] + 4*x[3]*x[6] - 3*x[4]^2 + 2*x[4]*x[5] + 4*x[4]*x[6] + 
+    19*x[5]^2 - 32*x[5]*x[6] + 10*x[6]^2 - x[7]^2 + 2*x[8]*x[9] + 4*x[9]^2,
+x[1]*x[2] + 2*x[2]*x[6] + 3*x[3]*x[4] - 3*x[3]*x[6] - 5*x[4]^2 - 2*x[4]*x[5] + 
+    6*x[4]*x[6] + 14*x[5]^2 - 26*x[5]*x[6] + 13*x[6]^2 - x[7]*x[8] + x[9]^2,
+x[1]*x[3] - 2*x[3]*x[6] - 2*x[4]^2 - x[4]*x[5] + x[4]*x[6] - 2*x[5]^2 + 
+    x[5]*x[6] + x[6]^2 - x[8]^2,
+x[1]*x[4] - 2*x[3]*x[4] + 3*x[4]^2 - 2*x[4]*x[6] - 9*x[5]^2 + 16*x[5]*x[6] - 
+    7*x[6]^2 - x[8]*x[9] - x[9]^2,
+x[1]*x[5] - 2*x[3]*x[4] + x[4]^2 + 2*x[4]*x[5] - 2*x[4]*x[6] - 6*x[5]^2 + 
+    10*x[5]*x[6] - 4*x[6]^2 - x[9]^2,
+x[1]*x[6] - x[3]*x[4] - x[3]*x[6] - x[4]^2 + x[4]*x[5] + x[4]*x[6] - x[5]^2 - 
+    x[5]*x[6] + 2*x[6]^2,
+x[1]*x[8] - x[2]*x[7] + x[5]*x[7] + 2*x[5]*x[8] - x[5]*x[9] - x[6]*x[7] + 
+    x[6]*x[9],
+x[1]*x[9] - x[3]*x[7] + x[4]*x[7] - x[5]*x[8] + 3*x[5]*x[9] + x[6]*x[7] + 
+    x[6]*x[8] - x[6]*x[9],
+x[2]^2 - 4*x[5]^2 + 8*x[5]*x[6] - 4*x[6]^2 - x[8]^2,
+x[2]*x[3] - 2*x[2]*x[6] - 2*x[3]*x[4] + 2*x[3]*x[6] + 4*x[4]^2 - 2*x[4]*x[5] - 
+    2*x[4]*x[6] - 8*x[5]^2 + 18*x[5]*x[6] - 10*x[6]^2 - x[8]*x[9] - x[9]^2,
+x[2]*x[4] - x[2]*x[6] - 2*x[3]*x[4] + 2*x[3]*x[6] + 2*x[4]^2 + 2*x[4]*x[5] - 
+    2*x[4]*x[6] - 8*x[5]^2 + 14*x[5]*x[6] - 8*x[6]^2 - x[9]^2,
+x[2]*x[5] - x[2]*x[6] - x[3]*x[4] + x[3]*x[6] + x[4]^2 - 2*x[5]^2 + 4*x[5]*x[6] 
+    - 3*x[6]^2,
+x[2]*x[8] - x[3]*x[7] - x[5]*x[8] + 2*x[6]*x[7] + x[6]*x[8],
+x[2]*x[9] - x[4]*x[7] + x[5]*x[7] - x[5]*x[9] + x[6]*x[9],
+x[3]^2 - 2*x[3]*x[4] - 2*x[3]*x[6] + x[4]^2 + 4*x[4]*x[5] - 2*x[4]*x[6] - 
+    8*x[5]^2 + 12*x[5]*x[6] - 3*x[6]^2 - x[9]^2,
+x[3]*x[5] - x[3]*x[6] - x[4]^2 + x[4]*x[5] + x[4]*x[6] - 3*x[5]*x[6] + 2*x[6]^2,
+x[3]*x[8] - x[4]*x[7] - x[5]*x[8] + x[6]*x[7] - x[6]*x[8],
+x[3]*x[9] - x[5]*x[7] - x[5]*x[9] + x[6]*x[7] - x[6]*x[9],
+x[4]*x[8] - x[5]*x[7] - x[5]*x[8] - x[5]*x[9] + x[6]*x[7] + x[6]*x[9],
+x[4]*x[9] - x[5]*x[8] + x[6]*x[8] - x[6]*x[9],
+x[7]*x[9] - x[8]^2 + x[8]*x[9] + x[9]^2
+
+w19 on X0(95) is given by:
+Mapping from: Crv: X95 to Crv: X95
+with equations : 
+u[1]
+u[2]
+u[3]
+u[4]
+u[5]
+u[6]
+-u[7]
+-u[8]
+-u[9]
+and inverse
+u[1]
+u[2]
+u[3]
+u[4]
+u[5]
+u[6]
+-u[7]
+-u[8]
+-u[9]
+
+Genus of X0(95) is  9
+Genus of X0(95)/w19 is  3
+
+By finding poles of j-map, we find that we have these 4 cusps:
+[
+    Place at (-3/5 : 0 : -2/5 : -1/5 : -1/5 : -1/5 : 1 : 0 : 0),
+    Place at (3/5 : 0 : 2/5 : 1/5 : 1/5 : 1/5 : 1 : 0 : 0),
+    Place at (-1 : 0 : 0 : 0 : 0 : 0 : 1 : 0 : 0),
+    Place at (1 : 0 : 0 : 0 : 0 : 0 : 1 : 0 : 0)
+]
+
+One quadratic but not rational poit is P5: (1/14*(-r19 - 17) : 1/7*(r19 - 11) : 
+1/7*(r19 + 3) : 1/7*(r19 - 4) : 1/14*(r19 + 3) : 1 : 0 : 0 : 0)
+Is P5 fixed by w19?
+true
+Hence, P5 can't come from a pullback of rational point.
+
+Is D1:=Divisor(P1) - Divisor(P3) of order 180?
+true
+
+Is D2:=Divisor(P1)-Divisor(P2)-10*D1 of order 6?
+true
+
+Is D2:=Divisor(P1)-Divisor(P2)-10*D1 of order 6 in J(Q)_tor/<D1>?
+true
+Hence, D2 and D1 generate a subgroup isomorphic to Z/6Z + Z/180Z
+
+J(Q)_tor is isomorphic to a subgroup of:
+Abelian Group isomorphic to Z/2 + Z/2 + Z/60 + Z/180
+Defined on 4 generators in supergroup CG:
+    JF3.1 = CG.1
+    JF3.2 = CG.2
+    JF3.3 = CG.3
+    JF3.4 = CG.4
+Relations:
+    2*JF3.1 = 0
+    2*JF3.2 = 0
+    60*JF3.3 = 0
+    180*JF3.4 = 0
+
+J(Q)_tor is isomorphic to a subgroup of:
+Abelian Group isomorphic to Z/2 + Z/2 + Z/2 + Z/2 + Z/2 + Z/18 + Z/90 + Z/900
+Defined on 8 generators in supergroup CG:
+    JF7.1 = CG.1
+    JF7.2 = CG.2
+    JF7.3 = CG.3
+    JF7.4 = CG.4
+    JF7.5 = CG.5
+    JF7.6 = CG.6
+    JF7.7 = CG.7
+    JF7.8 = CG.8
+Relations:
+    2*JF7.1 = 0
+    2*JF7.2 = 0
+    2*JF7.3 = 0
+    2*JF7.4 = 0
+    2*JF7.5 = 0
+    18*JF7.6 = 0
+    90*JF7.7 = 0
+    900*JF7.8 = 0
+Hence, J(Q)_tor doesn't have two independent elements of order 4.
+
+J(Q)_tor is isomorphic to a subgroup of:
+Abelian Group isomorphic to Z/4 + Z/12 + Z/36 + Z/1658340
+Defined on 4 generators in supergroup CG:
+    JF11.1 = CG.1
+    JF11.2 = CG.2
+    JF11.3 = CG.3
+    JF11.4 = CG.4
+Relations:
+    4*JF11.1 = 0
+    12*JF11.2 = 0
+    36*JF11.3 = 0
+    1658340*JF11.4 = 0
+Hence, J(Q)_tor doesn't have two independent elements of order 10.
+
+Since we have found a torsion subgroup iso. to Z/6Z + Z/180Z, and rk(J0(95)(Q)) 
+== 0,
+we know that J(Q) is a subgroup of:
+Z/2Z + Z/2Z + Z/6Z + Z/180Z
+
+We have found  11  points on X_0(95)^2(Q).
+2 of them are pullbacks of rationals from X0(95)/w19.
+9 of them are non-pullbacks
+Succeeded in proving that we have all exceptional quadratic pts? (true if 
+succeeded, number otherwise)
+11
+13
+13
+3
+11
+true
+
+
+Total time: 706.179 seconds, Total memory usage: 233.97MB
---
+++ X_0(95).m
@@ -0,0 +1,181 @@
+load "quadptssieve.m";
+load "X0N_NiceModel.m";
+
+//we find models for X0(95) and X0(95)/w19
+
+C:=CuspForms(95);
+"Dimension of CuspForms(95) is:";
+Dimension(C);
+
+w19:=AtkinLehnerOperator(C, 19);
+N19:=Nullspace(Matrix(w19 - 1));
+
+"Dimesion of eigenspace lambda=1 for w19 is:";
+Dimension(N19);
+
+N19c:=Nullspace(Matrix(w19 + 1));
+
+"Dimesion of eigenspace lambda=-1 for w19 is:";
+Dimension(N19c);
+
+B19:=[&+[(Integers()!(2*Eltseq(Basis(N19)[i])[j]))*C.j : j in [1..Dimension(C)]] : i in [1..Dimension(N19)]];
+B19c:=[&+[(Integers()!(2*Eltseq(Basis(N19c)[i])[j]))*C.j : j in [1..Dimension(C)]] : i in [1..Dimension(N19c)]];
+
+X95,jMap:= modformeqns(B19c cat B19, 95, 500, 19);
+"Model for X95 is:";
+X95;
+"";
+RR<[u]>:=CoordinateRing(AmbientSpace(X95));
+n:=Dimension(AmbientSpace(X95));
+
+H:=Matrix(RationalField(), 9, 9, [1,0,0,0,0,0,0,0,0, 0,1,0,0,0,0,0,0,0, 0,0,1,0,0,0,0,0,0, 0,0,0,1,0,0,0,0,0, 0,0,0,0,1,0,0,0,0, 0,0,0,0,0,1,0,0,0, 0,0,0,0,0,0,-1,0,0, 0,0,0,0,0,0,0,-1,0, 0,0,0,0,0,0,0,0,-1 ]);
+rows:=[[&+[RowSequence(H)[i][j]*u[j] : j in [1..n+1]] : i in [1..n+1]]] ;
+w19:=iso<X95->X95 | rows,rows>;
+"w19 on X0(95) is given by:";
+w19;
+"";
+
+X95w19, quotMap:=CurveQuotient(AutomorphismGroup(X95,[w19]));
+"Genus of X0(95) is ", Genus(X95);
+"Genus of X0(95)/w19 is ", Genus(X95w19);
+"";
+
+//some pts on X095
+K19<r19>:=QuadraticField(-19);
+
+P1:=X95![-1,0,0,0,0,0,1,0,0];
+P2:=X95![1,0,0,0,0,0,1,0,0];
+P3:=X95![-3/5,0,-2/5,-1/5,-1/5,-1/5,1,0,0];
+P4:=X95![3/5,0,2/5,1/5,1/5,1/5,1,0,0];
+
+"By finding poles of j-map, we find that we have these 4 cusps:";
+Poles(jMap);
+"";
+
+P5:=X95(K19)![1/14*(-r19-17), 1/7*(r19-11), 1/7*(r19+3), 1/7*(r19-4), 1/14*(r19+3),1,0,0,0];
+P6:=X95(K19)![1/14*(r19-17), 1/7*(-r19-11), 1/7*(-r19+3), 1/7*(-r19-4), 1/14*(-r19+3),1,0,0,0];
+
+"One quadratic but not rational poit is P5:", P5;
+"Is P5 fixed by w19?";
+w19(P5) eq P5;
+"Hence, P5 can't come from a pullback of rational point.";
+"";
+
+D1:=Divisor(P1) - Divisor(P3);
+bb, ff := IsPrincipal(180*D1);
+"Is D1:=Divisor(P1) - Divisor(P3) of order 180?";
+bb and not (IsPrincipal(90*D1) or IsPrincipal(60*D1) or IsPrincipal(36*D1));
+"";
+
+D2:=Divisor(P1)-Divisor(P2)-10*D1;
+"Is D2:=Divisor(P1)-Divisor(P2)-10*D1 of order 6?";
+bb, ff := IsPrincipal(6*D2);
+bb and not(IsPrincipal(3*D2) or IsPrincipal(2*D2));
+"";
+
+//since D2 is of order 6, its D1-part is 30*k*D1 for k in 0 to 5
+//3*D2 has D1-part equal to 0 or 90*D1
+//2*D2 has D1-part equal to 0, 60*D1 or -60*D1
+//we check that D1-free part of D2 is of order 6
+"Is D2:=Divisor(P1)-Divisor(P2)-10*D1 of order 6 in J(Q)_tor/<D1>?";
+not(IsPrincipal(3*D2) or IsPrincipal(3*D2 - 90*D1) or IsPrincipal(2*D2) or IsPrincipal(2*D2-60*D1) or IsPrincipal(2*D2 + 60*D1));
+"Hence, D2 and D1 generate a subgroup isomorphic to Z/6Z + Z/180Z";
+"";
+
+X3:=ChangeRing(X95,GF(3));
+CG,phi,psi:=ClassGroup(X3);
+Z:=FreeAbelianGroup(1);
+degr:=hom<CG->Z | [ Degree(phi(a))*Z.1 : a in OrderedGenerators(CG)]>;
+JF3:=Kernel(degr); // This is isomorphic to J_X(\F_3).
+"J(Q)_tor is isomorphic to a subgroup of:";
+JF3;
+"";
+
+X7:=ChangeRing(X95,GF(7));
+CG,phi,psi:=ClassGroup(X7);
+Z:=FreeAbelianGroup(1);
+degr:=hom<CG->Z | [ Degree(phi(a))*Z.1 : a in OrderedGenerators(CG)]>;
+JF7:=Kernel(degr); // This is isomorphic to J_X(\F_7).
+"J(Q)_tor is isomorphic to a subgroup of:";
+JF7;
+"Hence, J(Q)_tor doesn't have two independent elements of order 4.";
+"";
+
+X11:=ChangeRing(X95,GF(11));
+CG,phi,psi:=ClassGroup(X11);
+Z:=FreeAbelianGroup(1);
+degr:=hom<CG->Z | [ Degree(phi(a))*Z.1 : a in OrderedGenerators(CG)]>;
+JF11:=Kernel(degr); // This is isomorphic to J_X(\F_11).
+"J(Q)_tor is isomorphic to a subgroup of:";
+JF11;
+"Hence, J(Q)_tor doesn't have two independent elements of order 10.";
+"";
+
+"Since we have found a torsion subgroup iso. to Z/6Z + Z/180Z, and rk(J0(95)(Q)) == 0,";
+"we know that J(Q) is a subgroup of:";
+"Z/2Z + Z/2Z + Z/6Z + Z/180Z";
+"";
+
+//known degree 1 places
+pls1:=[Place(P1), Place(P2), Place(P3), Place(P4)];
+
+//known degree 2 places, we know P5 is not a pullback of a rational
+pls2:=[Place(P5)];
+
+deg2 := [1*pl : pl in pls2];
+deg2pb:=[];
+
+for i in [1..#pls1] do
+	for j in [i..#pls1] do
+		deg2:=Append(deg2, 1*pls1[i] + 1*pls1[j]);
+		if w19(RepresentativePoint(pls1[i])) eq RepresentativePoint(pls1[j]) then
+			deg2pb:=Append(deg2pb, 1*pls1[i] + 1*pls1[j]);
+		end if;
+	end for;
+end for;
+
+
+/*deg2:=[1*pl : pl in pls2] cat [1*pl1 + 1*pl2 : pl1 in pls1, pl2 in pls1];
+deg2pb:=[1*pl1 + 1*pl2 : pl1 in pls1, pl2 in pls1 | w19(RepresentativePoint(pl1)) eq RepresentativePoint(pl2)];*/
+
+deg2npb:=[DD : DD in deg2 | not DD in deg2pb];
+
+"We have found ", #deg2, " points on X_0(95)^2(Q).";
+#deg2pb, "of them are pullbacks of rationals from X0(95)/w19.";
+#deg2npb, "of them are non-pullbacks";
+
+//Finally, we do the sieve.
+A:=AbelianGroup([6, 180]);
+divs:=[D2, D1];
+genusC:=Genus(X95w19);
+auts:=[H];
+bp:=deg2pb[1];
+
+//We have shown that 2J(Q)_tor <= A
+I:=2;
+
+primes := [11, 13];
+
+"Succeeded in proving that we have all exceptional quadratic pts? (true if succeeded, number otherwise)";
+bret := MWSieve(deg2,primes,X95,A,divs,auts,genusC,deg2pb,deg2npb,I,bp);
+bret;
+assert bret eq true;
+"";
+
+/*
+_<x>:=PolynomialRing(Rationals());
+C:=HyperellipticCurve(x^8 - 2*x^7 - 7*x^6 + 16*x^5 - 2*x^4 -2*x^3 - 4*x^2 + 5);
+K<w>:=QuadraticField(5);
+X:=ChangeRing(C,K);
+P1:=PointsAtInfinity(X)[1];
+P2:=PointsAtInfinity(X)[2];
+P3:=X![1/2*(-w + 3), 1/2*(5*w - 7)];
+P4:=X![1/2*(w + 3), 1/2*(-5*w - 7)];
+D1:=P1-P2;
+D2:=(P3-P1)+(P4-P2);
+S:={};
+for i:=1 to 10 do for j:=1 to 10 do S:=S join {i*D1+j*D2}; end for; end for;
+#S;
+
+for a in S do R:=R join SequenceToSet(Roots(a[1])); end for;
+*/
