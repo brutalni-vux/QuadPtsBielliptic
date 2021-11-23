51b449ce7987ec431cc6e5a360889df1f6f02a6a
brutalni-vux <36900101+brutalni-vux@users.noreply.github.com>
2021-11-23 01:13:28 +0100

    Add files via upload

---
+++ Output_X_0(119).m
@@ -0,0 +1,209 @@
+Magma V2.26-3     Sun Nov 14 2021 14:05:27 on euler    [Seed = 902127086]
+Type ? for help.  Type <Ctrl>-D to quit.
+Loading "quadptssieve.m"
+Loading "ozmansiksek.m"
+Loading "X0N_NiceModel.m"
+Dimension of CuspForms(119) is:
+11
+Dimesion of eigenspace lambda=1 for w17 is:
+4
+Dimesion of eigenspace lambda=-1 for w17 is:
+7
+Model for X0(119) is:
+Curve over Rational Field defined by
+x[1]^2 + 8*x[2]*x[7] - 8*x[4]*x[5] + 24*x[4]*x[7] + 17*x[5]^2 - 24*x[5]*x[6] - 
+    4*x[5]*x[7] + 5*x[6]^2 + 38*x[6]*x[7] - 39*x[7]^2 - x[8]^2 + 2*x[10]^2 + 
+    2*x[10]*x[11] + 6*x[11]^2,
+x[1]*x[2] + 4*x[2]*x[7] - x[4]*x[5] + 2*x[4]*x[7] + 3*x[5]*x[6] - 4*x[6]^2 + 
+    2*x[6]*x[7] - 4*x[7]^2 - x[8]*x[9] + x[10]*x[11] + x[11]^2,
+x[1]*x[3] - 2*x[2]*x[7] + 4*x[3]*x[7] + 2*x[4]*x[5] - 6*x[4]*x[7] - 4*x[5]^2 + 
+    4*x[5]*x[6] + 2*x[5]*x[7] + 3*x[6]^2 - 14*x[6]*x[7] + 7*x[7]^2 - x[9]^2,
+x[1]*x[4] - 3*x[2]*x[7] + x[4]*x[5] - x[4]*x[7] - 3*x[5]^2 + 3*x[5]*x[6] - 
+    4*x[6]^2 + 2*x[6]*x[7] - 4*x[7]^2 - x[9]*x[10] - x[10]*x[11],
+x[1]*x[5] - 2*x[2]*x[7] + 3*x[4]*x[5] - 8*x[4]*x[7] - 4*x[5]^2 + 5*x[5]*x[6] + 
+    2*x[5]*x[7] + x[6]^2 - 12*x[6]*x[7] + 13*x[7]^2 - x[10]^2 - 2*x[11]^2,
+x[1]*x[6] - x[2]*x[7] - x[4]*x[7] + x[5]*x[6] - 3*x[5]*x[7] - x[6]^2 + 
+    4*x[6]*x[7] + x[7]^2 - x[10]*x[11],
+x[1]*x[7] - x[2]*x[7] + x[4]*x[5] - 3*x[4]*x[7] - 2*x[5]^2 + 3*x[5]*x[6] - 
+    6*x[6]*x[7] + 8*x[7]^2 - x[11]^2,
+x[1]*x[9] - x[2]*x[8] + x[6]*x[8] + 2*x[6]*x[9] - x[7]*x[8] + 2*x[7]*x[9],
+x[1]*x[10] - x[3]*x[8] + x[5]*x[8] + x[6]*x[9] + 4*x[6]*x[10] - x[6]*x[11] - 
+    x[7]*x[9] + x[7]*x[11],
+x[1]*x[11] - x[4]*x[8] + x[6]*x[8] - 2*x[6]*x[9] + x[6]*x[10] + 4*x[6]*x[11] + 
+    2*x[7]*x[8] + 2*x[7]*x[9] - x[7]*x[10],
+x[2]^2 + 4*x[6]^2 - 8*x[6]*x[7] + 4*x[7]^2 - x[9]^2,
+x[2]*x[3] - 2*x[2]*x[7] - x[5]^2 + x[5]*x[6] + 3*x[5]*x[7] - 3*x[6]^2 + 
+    4*x[6]*x[7] - 5*x[7]^2 - x[9]*x[10] - x[10]*x[11],
+x[2]*x[4] - 3*x[2]*x[7] + 3*x[4]*x[5] - 6*x[4]*x[7] - 3*x[5]^2 + 2*x[5]*x[6] + 
+    x[5]*x[7] + 3*x[6]^2 - 10*x[6]*x[7] + 13*x[7]^2 - x[10]^2 - 2*x[11]^2,
+x[2]*x[5] - 2*x[2]*x[7] + x[5]^2 - x[5]*x[6] - 3*x[5]*x[7] - x[6]^2 + 
+    4*x[6]*x[7] + x[7]^2 - x[10]*x[11],
+x[2]*x[6] - x[2]*x[7] + x[4]*x[5] - 2*x[4]*x[7] - x[5]^2 + 2*x[5]*x[6] - 
+    x[5]*x[7] + x[6]^2 - 6*x[6]*x[7] + 7*x[7]^2 - x[11]^2,
+x[2]*x[9] - x[3]*x[8] - x[6]*x[9] + 2*x[7]*x[8] + x[7]*x[9],
+x[2]*x[10] - x[4]*x[8] + x[6]*x[8] - 2*x[6]*x[9] + 2*x[6]*x[11] + 2*x[7]*x[8] + 
+    2*x[7]*x[9] - 2*x[7]*x[11],
+x[2]*x[11] - x[5]*x[8] - x[6]*x[9] - 2*x[6]*x[10] + 2*x[7]*x[8] + x[7]*x[9] + 
+    2*x[7]*x[10],
+x[3]^2 - 4*x[3]*x[7] + x[5]^2 - 2*x[5]*x[6] - 2*x[5]*x[7] + 2*x[6]^2 + 6*x[7]^2 
+    - x[10]^2 - 2*x[11]^2,
+x[3]*x[4] - 3*x[3]*x[7] - 2*x[4]*x[5] + 2*x[4]*x[7] + 3*x[5]^2 - 2*x[5]*x[6] - 
+    4*x[5]*x[7] - 3*x[6]^2 + 10*x[6]*x[7] - x[7]^2 - x[10]*x[11],
+x[3]*x[5] - 2*x[3]*x[7] + x[5]*x[6] - 3*x[5]*x[7] + x[6]^2 - 4*x[6]*x[7] + 
+    7*x[7]^2 - x[11]^2,
+x[3]*x[6] - x[3]*x[7] - x[4]*x[5] + 2*x[4]*x[7] + x[5]^2 - x[5]*x[6] - x[6]^2 + 
+    2*x[6]*x[7] - 3*x[7]^2,
+x[3]*x[9] - x[4]*x[8] - 3*x[6]*x[9] + 3*x[7]*x[8] + x[7]*x[9],
+x[3]*x[10] - x[5]*x[8] - x[6]*x[9] - 2*x[6]*x[10] + x[6]*x[11] + 2*x[7]*x[8] + 
+    x[7]*x[9] - x[7]*x[11],
+x[3]*x[11] - x[6]*x[8] - x[6]*x[9] - x[6]*x[10] - 2*x[6]*x[11] + x[7]*x[8] + 
+    x[7]*x[9] + x[7]*x[10],
+x[4]^2 - 6*x[4]*x[7] - 2*x[5]^2 + 6*x[5]*x[6] + 2*x[5]*x[7] - 3*x[6]^2 - 
+    6*x[6]*x[7] + 10*x[7]^2 - x[11]^2,
+x[4]*x[6] - x[4]*x[7] - x[5]^2 + x[5]*x[6] + 3*x[5]*x[7] - 2*x[6]^2 - x[6]*x[7] 
+    - x[7]^2,
+x[4]*x[9] - x[5]*x[8] - 2*x[6]*x[9] + 2*x[7]*x[8] - x[7]*x[9],
+x[4]*x[10] - x[6]*x[8] - x[6]*x[9] - x[6]*x[10] + x[6]*x[11] + x[7]*x[8] + 
+    x[7]*x[9] - 2*x[7]*x[10] - x[7]*x[11],
+x[4]*x[11] - x[6]*x[9] - x[6]*x[10] - x[6]*x[11] + x[7]*x[9] + x[7]*x[10] - 
+    2*x[7]*x[11],
+x[5]*x[9] - x[6]*x[8] - x[6]*x[9] + x[7]*x[8] - x[7]*x[9],
+x[5]*x[10] - x[6]*x[9] - x[6]*x[10] + x[6]*x[11] + x[7]*x[9] - x[7]*x[10] - 
+    x[7]*x[11],
+x[5]*x[11] - x[6]*x[10] - x[6]*x[11] + x[7]*x[10] - x[7]*x[11],
+x[8]*x[10] - x[9]^2 + x[10]^2 + x[11]^2,
+x[8]*x[11] - x[9]*x[10],
+x[9]*x[11] - x[10]^2 - x[11]^2
+
+w17 on X0(119) is given by:
+Mapping from: Crv: X119 to Crv: X119
+with equations : 
+u[1]
+u[2]
+u[3]
+u[4]
+u[5]
+u[6]
+u[7]
+-u[8]
+-u[9]
+-u[10]
+-u[11]
+and inverse
+u[1]
+u[2]
+u[3]
+u[4]
+u[5]
+u[6]
+u[7]
+-u[8]
+-u[9]
+-u[10]
+-u[11]
+
+Genus of X0(119) is  11
+Genus of X0(119)/w17 is  4
+
+By finding poles of j-map, we find that we have these 4 cusps:
+[
+    Place at (-3/7 : 0 : -2/7 : -3/7 : -2/7 : -1/7 : -1/7 : 1 : 0 : 0 : 0),
+    Place at (3/7 : 0 : 2/7 : 3/7 : 2/7 : 1/7 : 1/7 : 1 : 0 : 0 : 0),
+    Place at (1 : 0 : 0 : 0 : 0 : 0 : 0 : 1 : 0 : 0 : 0),
+    Place at (-1 : 0 : 0 : 0 : 0 : 0 : 0 : 1 : 0 : 0 : 0)
+]
+
+and these 2 non-cusps (plus Galois conjugates):
+P5:  (1/7*(-2*rt + 1) : 0 : 1/7*(rt - 4) : 1/14*(3*rt - 19) : 1/7*(rt + 3) : 
+1/14*(rt - 11) : 1/14*(rt + 3) : -2 : 2 : -1 : 1)
+P6:  (1/7*(2*rt - 1) : 0 : 1/7*(-rt + 4) : 1/14*(-3*rt + 19) : 1/7*(-rt - 3) : 
+1/14*(-rt + 11) : 1/14*(-rt - 3) : -2 : 2 : -1 : 1)
+
+P5 and P6 are not pullbacks of rationals, they reduce via quotMap to:
+(1 : 1/2*(-rt - 3) : 1) (1 : 1/2*(-rt - 3) : 1)
+
+J0(119)(Q)_tor is isomorphic to a subgroup of:
+Abelian Group isomorphic to Z/8 + Z/94752
+Defined on 2 generators in supergroup Cg:
+    JF3.1 = Cg.1
+    JF3.2 = Cg.2
+Relations:
+    8*JF3.1 = 0
+    94752*JF3.2 = 0
+
+J0(119)(Q)_tor is isomorphic to a subgroup of:
+Abelian Group isomorphic to Z/2 + Z/2 + Z/16 + Z/2596896
+Defined on 4 generators in supergroup Cg:
+    JF5.1 = Cg.1
+    JF5.2 = Cg.2
+    JF5.3 = Cg.3
+    JF5.4 = Cg.4
+Relations:
+    2*JF5.1 = 0
+    2*JF5.2 = 0
+    16*JF5.3 = 0
+    2596896*JF5.4 = 0
+
+By injecting torsion mod 3 and mod 5, we see that torsion has at most  2304  
+elements.
+
+Clearly, cuspidal group is generated by Di := P1 - Pi, i in {2,3,4}
+
+Is D3 of order 288?
+true
+
+Is Dt := 9*D4 of order 8?
+true
+
+Is 4*Dt of order 2 in J(Q)_tor/<D3>?
+true
+Therefore, Dt := 9*D4 and D3 generate a torsion subgroup of order 288*8 = 2304, 
+hence the whole torsion
+We have found  12  points on X_0(119)^2(Q).
+2 of them are pullbacks of rationals from X0(95)/w19.
+10 of them are non-pullbacks
+5
+5
+Succeeded in proving that we have all exceptional quadratic pts? (true if 
+succeeded, number otherwise)
+true
+
+>> assert bret eq ture;
+                  ^
+User error: Identifier 'ture' has not been declared or assigned
+This means we have found all exceptional quadratic points on X0(119).
+Now we only need to find rational points on X0(119)/w17 and check their 
+pullbacks.
+X0(119)/w17 is actually  Hyperelliptic Curve defined by y^2 = x^10 + 2*x^8 - 
+    11*x^6 + 14*x^5 - 40*x^4 + 42*x^3 - 48*x^2 + 28*x - 7 over Rational Field
+Rank of J(X0(119)/w17)(Q) is 0
+Hence rk(J(X0(119)/w17)(Q)) = rk(J0(119)(Q)) = 0.
+
+J(X0(95)/w19)(Q)_tors has at most  9  points.
+We have found  9  points on J(X0(119)/w17)(Q) out of possible  9
+Hence, we know whole J(X0(119)/w17)(Q).
+
+Using Mumford representations, we get that there are no rational points on 
+X0(119)/w17
+(except pts at infinity).
+
+Hence, only rational points are points at infinity.
+
+Pullback of point  (1 : -1 : 0)  is:
+[
+    <Place at (-3/7 : 0 : -2/7 : -3/7 : -2/7 : -1/7 : -1/7 : 1 : 0 : 0 : 0), 1>,
+    <Place at (3/7 : 0 : 2/7 : 3/7 : 2/7 : 1/7 : 1/7 : 1 : 0 : 0 : 0), 1>
+]
+Pullback of point  (1 : 1 : 0)  is:
+[
+    <Place at (1 : 0 : 0 : 0 : 0 : 0 : 0 : 1 : 0 : 0 : 0), 1>,
+    <Place at (-1 : 0 : 0 : 0 : 0 : 0 : 0 : 1 : 0 : 0 : 0), 1>
+]
+Hence, the only quadratic point up to Galois conjugacy, apart from rational 
+cusps, are:
+(1/7*(-2*rt + 1) : 0 : 1/7*(rt - 4) : 1/14*(3*rt - 19) : 1/7*(rt + 3) : 1/14*(rt
+    - 11) : 1/14*(rt + 3) : -2 : 2 : -1 : 1) (1/7*(2*rt - 1) : 0 : 1/7*(-rt + 4)
+: 1/14*(-3*rt + 19) : 1/7*(-rt - 3) : 1/14*(-rt + 11) : 1/14*(-rt - 3) : -2 : 2 
+: -1 : 1)
+
+Total time: 80704.770 seconds, Total memory usage: 226.44MB
