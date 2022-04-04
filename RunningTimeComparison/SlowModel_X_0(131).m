513d5ecd9f1613df2d18b40ea16a8bb4f3fd618e
Borna Vukorepa <36900101+brutalni-vux@users.noreply.github.com>
2022-04-04 19:45:19 +0200

    Create SlowModel_X_0(131).m

---
+++ RunningTimeComparison/SlowModel_X_0(131).m
@@ -0,0 +1,11 @@
+load "X0p_NiceModel.m";
+load "Chabauty_MWSieve_131.m";
+
+//we compute the model for X_0(131) using the default cuspform basis in Magma
+
+C := CuspForms(131);
+"Dimension of CuspForms(131) is: ", Dimension(C);
+
+X131 := modformeqns(Basis(C), 131, 500, 1);
+"Nice model for X0(131) is:";
+X131;
