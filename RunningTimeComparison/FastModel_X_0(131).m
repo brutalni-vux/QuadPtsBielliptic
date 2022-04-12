6c395953566911e7f41497bc429c0de596cd9362
Borna Vukorepa <36900101+brutalni-vux@users.noreply.github.com>
2022-04-12 21:54:22 +0200

    Update FastModel_X_0(131).m

--- RunningTimeComparison/FastModel_X_0(131).m
+++ RunningTimeComparison/FastModel_X_0(131).m
@@ -1,6 +1,7 @@
 load "X0p_NiceModel.m";
+load "Chabauty_MWSieve_131.m";
 
-//compute model for X_0(131) using the diagonal cuspform basis (such that the Atkin-Lehner matrix is diagonal)
+//we find models for X131 and X131/w131
 
 C := CuspForms(131);
 "Dimension of CuspForms(131) is: ", Dimension(C);
@@ -21,3 +22,32 @@ B131c := [&+[(Integers()!(2*Eltseq(Basis(N131c)[i])[j]))*C.j : j in [1..Dimensio
 X131 := modformeqns(B131c cat B131, 131, 500, 1);
 "Nice model for X0(131) is:";
 X131;
+
+"";
+RR<[u]> := CoordinateRing(AmbientSpace(X131));
+n := Dimension(AmbientSpace(X131));
+
+H := Matrix(RationalField(), 11, 11, [1,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,-1]);
+rows := [[&+[RowSequence(H)[i][j]*u[j] : j in [1..n+1]] : i in [1..n+1]]] ;
+w131 := iso<X131 -> X131 | rows, rows>;
+"w131 on X131 is given by:";
+w131;
+"";
+
+
+X131w131, quotMap := Curve_and_Map(X131, 10);
+
+P1 := X131![1,0,0,0,0,0,0,0,0,0,1];
+P2 := X131![-1,0,0,0,0,0,0,0,0,0,1];
+
+pts := [P1, P2];
+
+E, eMap := EllipticCurve(X131w131, quotMap(pts[1]));
+"X0(131)/w131 is actually the following elliptic curve:";
+E;
+"";
+
+mp := quotMap*eMap;
+
+"Quotient map is:";
+mp;
